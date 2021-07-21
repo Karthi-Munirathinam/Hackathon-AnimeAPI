faa1878cada74a8f1a857b49f8e828938da31202
Karthi M <83709085+Karthi-Munirathinam@users.noreply.github.com>
2021-07-21 12:58:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -33,105 +33,230 @@ searchIcon.append(iconImg);
 form.append(searchInput, searchIcon);
 div.append(form);
 nav.append(a, div);
-const br = document.createElement('br');
 
 
-//Search Results COntainer
+//Search Results Container
 
 const container = document.createElement('div');
 container.classList.add('container');
-document.body.append(nav, br, container);
+document.body.append(nav, container);
 const row = document.createElement('div');
 row.setAttribute('class', 'row main');
 container.append(row);
 
 function createCard(data) {
-    let url = data.image_url;
-    let startdate = data.start_date;
-    let endDate = data.end_date;
-    let type = data.type;
-    let IMDb = data.score;
-    let Episodes = data.episodes;
-    let rated = data.rated;
-    let Title = data.title;
-    let startyear = Year(startdate);
-    let endyear = Year(endDate);
-    const col = document.createElement('div');
-    col.setAttribute('class', 'col-md-4 col-lg-3 p-3');
-    row.append(col);
-    const flipCard = document.createElement('div');
-    flipCard.classList.add('flip-card');
-    col.append(flipCard);
-    const innerCard = document.createElement('div');
-    innerCard.setAttribute('class', 'row card-image card-inner');
-    flipCard.append(innerCard);
-    //Front Card:
-    const cardFront = document.createElement('div');
-    cardFront.setAttribute('class', 'col-12 card-front');
-    const imgFront = document.createElement('img');
-    imgFront.setAttribute('class', 'card-img');
-    imgFront.setAttribute('width', '200px');
-    imgFront.setAttribute('height', '250px');
-    imgFront.setAttribute('alt', 'poster');
-    imgFront.setAttribute('src', url);
-    const hrline = document.createElement('hr');
-    hrline.classList.add('hrline');
-    const title = document.createElement('div');
-    title.setAttribute('class', 'card-title title');
-    title.innerText = Title;
-    cardFront.append(imgFront, hrline, title);
-    //Back Card
-    const cardBack = document.createElement('div');
-    cardBack.setAttribute('class', 'col-10 card-back');
-    const imgBack = document.createElement('img');
-    imgBack.setAttribute('class', 'card-img ');
-    imgBack.setAttribute('width', '200px');
-    imgBack.setAttribute('height', '250px');
-    imgBack.setAttribute('alt', 'poster');
-    imgBack.setAttribute('src', url);
-    imgBack.setAttribute('style', "opacity: 0.2;");
-    const imageOverlay = document.createElement('div');
-    imageOverlay.classList.add('card-img-overlay')
-    const p1 = backCardContent('p', "card-text", "Episodes: " + Episodes);
-    const p2 = backCardContent('p', "card-text", type + " | " + rated);
-    const p3 = backCardContent('p', "card-text", "IMDb Rating: " + IMDb);
-    const p4 = backCardContent('p', "card-text", "Start Date: " + startyear);
-    const p5 = backCardContent('p', "card-text", "End Date: " + endyear);
-    imageOverlay.append(p1, p2, p3, p4, p5);
-    cardBack.append(imgBack, imageOverlay);
-    innerCard.append(cardFront, cardBack);
+    try {
+        let url = data.image_url;
+        let startdate = data.start_date;
+        let endDate = data.end_date;
+        let type = data.type;
+        let IMDb = data.score;
+        let Episodes = data.episodes;
+        let rated = data.rated;
+        let Title = data.title;
+        let startyear = Year(startdate);
+        let endyear = Year(endDate);
+        let mal_id = data.mal_id;
+        const col = document.createElement('div');
+        col.setAttribute('class', 'col-md-4 col-lg-3 mb-2 mt-2');
+        row.append(col);
+
+        //poster
+        let poster_div = document.createElement('div');
+        poster_div.setAttribute('class', 'poster');
+        const img = document.createElement('img');
+        img.setAttribute('src', url);
+        img.setAttribute('alt', 'poster');
+        img.classList.add('img-fluid');
+        const info = document.createElement('button');
+        info.classList.add('info');
+        info.setAttribute('onclick', 'displayContents(id)');
+        info.setAttribute('id', "c" + mal_id);
+        info.innerText = "View more";
+        poster_div.append(img, info);
+
+
+        //content
+        const content_div = document.createElement('div');
+        content_div.classList.add('content');
+        content_div.classList.add('c' + mal_id);
+        const episodeInfo = document.createElement('p');
+        episodeInfo.innerText = "Episodes : " + Episodes;
+        const typeInfo = document.createElement('p');
+        typeInfo.innerText = type + " | " + rated;
+        const IMDbInfo = document.createElement('p');
+        IMDbInfo.innerText = "IMDb rating : " + IMDb + " / 10";
+        const StartDateInfo = document.createElement('p');
+        StartDateInfo.innerText = "Start-Date : " + startyear;
+        const EndDateInfo = document.createElement('p');
+        EndDateInfo.innerText = "End-Date : " + endyear;
+        content_div.append(episodeInfo, typeInfo, IMDbInfo, StartDateInfo, EndDateInfo);
+
+
+        //Title
+        const title_div = document.createElement('div');
+        title_div.classList.add("title");
+        title_div.innerText = Title;
+        col.append(poster_div, content_div, title_div);
+    } catch (error) {
+        console.log('error in creating cards data ' + error);
+    }
 }
 
-function backCardContent(htmlele, classes, contents) {
-    const p = document.createElement(htmlele);
-    p.setAttribute('class', classes);
-    p.innerText = contents;
-    return p;
-}
+let count = 0;
 
 async function searchResults() {
-    row.innerText = '';
-    let searchstring = document.getElementById('search').value;
-    let tosearch = searchstring.replace(/[ ]/g, '%20');
-    let jsondata = await fetch(`https://api.jikan.moe/v3/search/anime?q=${tosearch}`);
-    let data = await jsondata.json();
-    // console.log(data.results);
-    displayData(data.results);
+    try {
+        var current = 1;
+        let searchstring = document.getElementById('search').value;
+        let tosearch = searchstring.replace(/[ ]/g, '%20');
+        let jsondata = await fetch(`https://api.jikan.moe/v3/search/anime?q=${tosearch}`);
+        let data = await jsondata.json();
+        let purl = `https://api.jikan.moe/v3/search/anime?q=${tosearch}`;
+        displayData(data.results);
+        pagenation(data, purl, current);
+        count++;
+    } catch (error) {
+        console.log('error in SearchResults data ' + error);
+    }
+
 }
 
 function displayData(data) {
-    data.forEach((element) => {
-        createCard(element);
-    })
+    try {
+        row.innerText = '';
+        data.forEach((element) => {
+            createCard(element);
+        })
+    } catch (error) {
+        console.log('error in displayData data ' + error);
+    }
+
 }
 
 function Year(date) {
-    let DATE = new Date(date);
-    const year = DATE.getFullYear();
-    const month = DATE.getMonth() + 1;
-    const day = DATE.getDate();
-    return day + "-" + month + "-" + year;
+    try {
+        let DATE = new Date(date);
+        const year = DATE.getFullYear();
+        const month = DATE.getMonth() + 1;
+        const day = DATE.getDate();
+        return day + " / " + month + " / " + year;
+    } catch (error) {
+        console.log('error in converting year data ' + error);
+    }
+
+}
+
+//displayContents
+
+function displayContents(value) {
+    try {
+        const button_con = document.getElementById(value);
+        const dcontent = document.getElementsByClassName(value);
+        for (i = 0; i < dcontent.length; i++) {
+            if (dcontent[i].style.display == "block") {
+                dcontent[i].style.display = "none";
+                button_con.innerText = "View more";
+            } else {
+                button_con.innerText = "View less";
+                dcontent[i].style.display = "block";
+
+            }
+        }
+
+    } catch (error) {
+        console.log('error in displayContents function ' + error);
+    }
+
+
 }
 
 
+//Pagenation
+async function pagenation(data, purl, current) {
+    try {
+        if (count > 0) {
+            removePagenation();
+        }
+        const lastpage = data.last_page;
+        const pcontainer = document.createElement('div');
+        pcontainer.classList.add('container');
+        pcontainer.setAttribute('id', 'pcontainer');
+        const prow = document.createElement('div');
+        prow.classList.add('row');
+        const pcol = document.createElement('div');
+        pcol.setAttribute('class', 'col-md-6 pcol');
+        pcontainer.append(prow, pcol);
+        document.body.append(pcontainer);
+        const previous = document.createElement('div');
+        previous.classList.add('previous');
+        const pbutton = document.createElement('button');
+        pbutton.setAttribute('class', 'btn btn-outline-primary previousbtn');
+        pbutton.setAttribute('data-url', purl);
+        pbutton.setAttribute('data-current', current);
+        pbutton.setAttribute('onclick', 'previouspage(this)');
+        pbutton.innerText = "Previous";
+        previous.append(pbutton);
+        const pspan = document.createElement('span');
+        pspan.classList.add('number');
+        pspan.innerText = current + " / " + lastpage;
+        const next = document.createElement('div');
+        next.classList.add('Next');
+        const nbutton = document.createElement('button');
+        nbutton.setAttribute('class', 'btn btn-outline-secondary Nextbtn');
+        nbutton.innerText = "Next";
+        nbutton.setAttribute('data-url', purl);
+        nbutton.setAttribute('data-current', current);
+        nbutton.setAttribute('data-lastpage', lastpage);
+        nbutton.setAttribute('onclick', 'nextpage(this)');
+        next.append(nbutton);
+        pcol.append(previous, pspan, next);
+    } catch (error) {
+        console.log('error in pagination data ' + error);
+    }
+
+
+
+}
+
+async function nextpage(values) {
+    try {
+        let ncurrent = values.getAttribute('data-current');
+        let pagurl = values.getAttribute('data-url');
+        let lastPage = values.getAttribute('data-lastpage');
+        let ncount = +ncurrent + 1;
+        if (ncount < lastPage) {
+            let nextdata = await fetch(pagurl + "&page=" + +ncount);
+            let ndata = await nextdata.json();
+            displayData(ndata.results);
+            pagenation(ndata, pagurl, ncount);
+        }
+    } catch (error) {
+        console.log('error in next page data ' + error);
+    }
+
+
+}
+async function previouspage(values) {
+    try {
+        let pcurrent = values.getAttribute('data-current');
+        let pagurl = values.getAttribute('data-url');
+        let pcount = +pcurrent - 1;
+        if (pcount > 0) {
+            let previousdata = await fetch(pagurl + "&page=" + +pcount);
+            let pdata = await previousdata.json();
+            displayData(pdata.results);
+            pagenation(pdata, pagurl, pcount);
+        }
+    } catch (error) {
+        console.log('error in previous page data ' + error);
+    }
+
+
+}
+
+function removePagenation() {
+    const pcontainer = document.getElementById('pcontainer');
+    pcontainer.remove();
+}
 
